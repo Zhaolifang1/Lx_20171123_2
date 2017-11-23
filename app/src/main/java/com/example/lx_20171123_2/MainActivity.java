38e95d69b7d5dab1c7f869328fef2a924889be99
Zhaolifang1 <2322344659@qq.com>
2017-11-23 11:34:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lx_20171123_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lx_20171123_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ lianxi/src/main/java/text/bwei/com/yuekao08lianxi/model/Imodel.java
@@ -0,0 +1,10 @@
+package text.bwei.com.yuekao08lianxi.model;
+
+/**
+ * Created by dell on 2017/11/23.
+ */
+
+public interface Imodel {
+    void RequestSuccess(String url,OnselectLick onselectLick);
+
+}
